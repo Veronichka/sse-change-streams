b62da32e46d6cf5e4b50e2a223df9cbc440c6309
vherasymenko <vherasymenko@sciforce.solutions>
2018-05-26 17:10:43 -0400

    Initial commit.

---
+++ src/main/groovy/com/herasymenko/ssechangestream/SseChangeStreamApplication.groovy
@@ -0,0 +1,12 @@
+package com.herasymenko.ssechangestream
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SseChangeStreamApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SseChangeStreamApplication, args
+	}
+}
